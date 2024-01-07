eb996fa1d4e73cfb8fab3b898064cf4f6fd04430
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-01-07 15:32:35 +0100

    [ADD] program discord channels enum

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,6 +13,15 @@ const program = defineCollection({
       "Hackathon",
       "Visioconférence",
     ]),
+    discord: z.enum([
+      "ef-01-02-2024",
+      "cm-29-04-2024",
+      "sc-02-05-2024",
+      "jp-04-05-2024",
+      "xp-05-06-2024",
+      "hackathon",
+      "data-challenge",
+    ]),
     place: z.string(),
     description: z.string(),
   })
