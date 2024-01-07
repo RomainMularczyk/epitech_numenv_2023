1643cd969de2238bad8b6c2f76a36574a39b3520
Romain Mularczyk <romain.mularczyk@gmail.com>
2024-01-07 15:09:51 +0100

    [DEL] svelte integration
    Modify Boavizta and remove capital letters

--- astro.config.ts
+++ astro.config.ts
@@ -7,6 +7,8 @@ import prefetch from "@astrojs/prefetch";
 import remarkUnwrapImages from "remark-unwrap-images";
 import { remarkReadingTime } from "./src/utils/remark-reading-time";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
